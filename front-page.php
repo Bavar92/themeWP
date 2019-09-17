b760573b25e2448fb25dd9fd99fa401f1e5ab753
unknown <vasilij6090@gmail.com>
2019-09-17 15:48:03 +0300

    if get post empty echo false

--- front-page.php
+++ front-page.php
@@ -1,9 +1,14 @@
 <?php get_header(); ?>
 <section class="content">
     <div class="container">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <?php the_content(); ?>
-        <?php endwhile; endif; ?>
+        <?php
+        if ('' !== get_post()->post_content) { ?>
+            <div class="wysiwyg">
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <?php the_content(); ?>
+                <?php endwhile; endif; ?>
+            </div>
+        <?php } ?>
     </div>
 </section>
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -5,10 +5,11 @@
             <h1><?php the_title(); ?></h1>
             <?php
             if ('' !== get_post()->post_content) { ?>
-                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                    <?php the_content(); ?>
-                <?php endwhile; endif; ?>
-
+                <div class="wysiwyg">
+                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                        <?php the_content(); ?>
+                    <?php endwhile; endif; ?>
+                </div>
             <?php } ?>
             <a class="button" href="<?php echo get_permalink(BLOG_ID); ?>">Go back</a>
         </article>
